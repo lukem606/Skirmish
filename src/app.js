b1098b6a3d6cfa8f3c0e609c21d04a142b8b09e6
Luke Martin <lukemartin@Lukes-MacBook-Pro.local>
2021-12-01 16:55:03 +0000

    Unit movement in place? Work needed on Platoon. Engine and Game files added. Vector has more complete methods. Map class added, needs to be utilised.

--- src/app.js
+++ src/app.js
@@ -1,39 +1,5 @@
-import initialise from "./init.js";
-import Unit from "./engine/unit.js";
-import Utils from "./utils/mathUtils.js";
+import Game from "./engine/game.js";
 import "./style.css";
-import Vector from "./utils/vector.js";
 
-const setup = () => {
-  return initialise();
-};
-
-const draw = () => {
-  context.clearRect(0, 0, WIDTH, HEIGHT);
-
-  units.forEach((unit) => {
-    unit.update();
-    unit.checkEdges(canvas);
-    unit.render(context);
-  });
-
-  requestAnimationFrame(draw);
-};
-
-global = setup();
-const { canvas, context, WIDTH, HEIGHT } = global;
-
-const units = [];
-
-for (let i = 0; i < 5; i++) {
-  const unit = new Unit(50 + i * 15, 50);
-
-  const velocity = new Vector(1, 2);
-  velocity.normalise();
-  velocity.mult(unit.stats.speed);
-
-  unit.velocity = velocity;
-  units.push(unit);
-}
-
-draw();
+const game = new Game();
+game.run();
---
+++ src/engine/game.js
@@ -0,0 +1,88 @@
+import Vector from "../utils/vector.js";
+import Map from "./map.js";
+import MathUtils from "../utils/mathUtils.js";
+import Platoon from "./platoon.js";
+import Unit from "./unit.js";
+
+export default class Game {
+  constructor() {
+    this.initialiseCanvas(20);
+    this.initialiseMap();
+    // this.initialiseUnits();
+    this.initialisePlatoons();
+  }
+
+  run() {
+    this.draw();
+    requestAnimationFrame(this.run.bind(this));
+  }
+
+  initialiseCanvas(pad) {
+    const WIDTH = window.innerWidth - pad * 2;
+    const HEIGHT = window.innerHeight - pad * 2;
+
+    const container = document.createElement("div");
+    container.id = "container";
+    container.style.padding = `${pad}px`;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    container.appendChild(canvas);
+    document.body.appendChild(container);
+
+    this.canvas = document.querySelector("canvas");
+    this.context = canvas.getContext("2d");
+
+    global = {
+      canvas: this.canvas,
+      context: this.context,
+      WIDTH: WIDTH,
+      HEIGHT: HEIGHT,
+    };
+  }
+
+  initialiseMap() {
+    this.map = new Map();
+  }
+
+  initialiseUnits() {
+    const units = [];
+
+    for (let i = 0; i < 5; i++) {
+      const unit = new Unit(
+        Math.random() * global.WIDTH,
+        Math.random() * global.HEIGHT
+      );
+      units.push(unit);
+    }
+
+    this.units = units;
+  }
+
+  initialisePlatoons() {
+    const platoons = [];
+
+    for (let i = 0; i < 5; i++) {
+      const origin = new Vector(
+        MathUtils.random(50, global.WIDTH - 50),
+        MathUtils.random(50, global.HEIGHT - 50)
+      );
+
+      platoons.push(new Platoon(origin.x, origin.y));
+    }
+
+    this.platoons = platoons;
+  }
+
+  draw() {
+    this.context.clearRect(0, 0, global.WIDTH, global.HEIGHT);
+
+    this.platoons.forEach((platoon) => {
+      platoon.update();
+      platoon.checkEdges();
+      platoon.render();
+    });
+  }
+}
